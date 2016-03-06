23bbc9ebde58c8a75b2ca0c968f0231a70f20dd0
mouson <mouson@gmail.com>
2016-03-07 01:01:48 +0800

    修正範例資料使用錯誤

--- tests/GroupSplitTool/GroupSplitToolTest.php
+++ tests/GroupSplitTool/GroupSplitToolTest.php
@@ -12,17 +12,17 @@ class GroupSplitToolTest extends PHPUnit_Framework_TestCase
     {
         /** Arrange */
         $sample_data = [
-            ['Id' => 1 , 'Cost' => 1, 'Revenue' => 11, 'SellPrice' => 21],
-            ['Id' => 2 , 'Cost' => 2, 'Revenue' => 12, 'SellPrice' => 22],
-            ['Id' => 3 , 'Cost' => 3, 'Revenue' => 13, 'SellPrice' => 23],
-            ['Id' => 4 , 'Cost' => 4, 'Revenue' => 14, 'SellPrice' => 24],
-            ['Id' => 5 , 'Cost' => 5, 'Revenue' => 15, 'SellPrice' => 25],
-            ['Id' => 6 , 'Cost' => 6, 'Revenue' => 16, 'SellPrice' => 26],
-            ['Id' => 7 , 'Cost' => 7, 'Revenue' => 17, 'SellPrice' => 27],
-            ['Id' => 8 , 'Cost' => 8, 'Revenue' => 18, 'SellPrice' => 28],
-            ['Id' => 9 , 'Cost' => 9, 'Revenue' => 19, 'SellPrice' => 29],
-            ['Id' => 10, 'Cost' => 1, 'Revenue' => 20, 'SellPrice' => 30],
-            ['Id' => 11, 'Cost' => 1, 'Revenue' => 21, 'SellPrice' => 31],
+            ['Id' => 1,  'Cost' => 1,  'Revenue' => 11, 'SellPrice' => 21],
+            ['Id' => 2,  'Cost' => 2,  'Revenue' => 12, 'SellPrice' => 22],
+            ['Id' => 3,  'Cost' => 3,  'Revenue' => 13, 'SellPrice' => 23],
+            ['Id' => 4,  'Cost' => 4,  'Revenue' => 14, 'SellPrice' => 24],
+            ['Id' => 5,  'Cost' => 5,  'Revenue' => 15, 'SellPrice' => 25],
+            ['Id' => 6,  'Cost' => 6,  'Revenue' => 16, 'SellPrice' => 26],
+            ['Id' => 7,  'Cost' => 7,  'Revenue' => 17, 'SellPrice' => 27],
+            ['Id' => 8,  'Cost' => 8,  'Revenue' => 18, 'SellPrice' => 28],
+            ['Id' => 9,  'Cost' => 9,  'Revenue' => 19, 'SellPrice' => 29],
+            ['Id' => 10, 'Cost' => 10, 'Revenue' => 20, 'SellPrice' => 30],
+            ['Id' => 11, 'Cost' => 11, 'Revenue' => 21, 'SellPrice' => 31],
         ];
 
         $row_per_group = 3;
@@ -48,17 +48,17 @@ class GroupSplitToolTest extends PHPUnit_Framework_TestCase
     {
         /** Arrange */
         $sample_data = [
-            ['Id' => 1 , 'Cost' => 1, 'Revenue' => 11, 'SellPrice' => 21],
-            ['Id' => 2 , 'Cost' => 2, 'Revenue' => 12, 'SellPrice' => 22],
-            ['Id' => 3 , 'Cost' => 3, 'Revenue' => 13, 'SellPrice' => 23],
-            ['Id' => 4 , 'Cost' => 4, 'Revenue' => 14, 'SellPrice' => 24],
-            ['Id' => 5 , 'Cost' => 5, 'Revenue' => 15, 'SellPrice' => 25],
-            ['Id' => 6 , 'Cost' => 6, 'Revenue' => 16, 'SellPrice' => 26],
-            ['Id' => 7 , 'Cost' => 7, 'Revenue' => 17, 'SellPrice' => 27],
-            ['Id' => 8 , 'Cost' => 8, 'Revenue' => 18, 'SellPrice' => 28],
-            ['Id' => 9 , 'Cost' => 9, 'Revenue' => 19, 'SellPrice' => 29],
-            ['Id' => 10, 'Cost' => 1, 'Revenue' => 20, 'SellPrice' => 30],
-            ['Id' => 11, 'Cost' => 1, 'Revenue' => 21, 'SellPrice' => 31],
+            ['Id' => 1,  'Cost' => 1,  'Revenue' => 11, 'SellPrice' => 21],
+            ['Id' => 2,  'Cost' => 2,  'Revenue' => 12, 'SellPrice' => 22],
+            ['Id' => 3,  'Cost' => 3,  'Revenue' => 13, 'SellPrice' => 23],
+            ['Id' => 4,  'Cost' => 4,  'Revenue' => 14, 'SellPrice' => 24],
+            ['Id' => 5,  'Cost' => 5,  'Revenue' => 15, 'SellPrice' => 25],
+            ['Id' => 6,  'Cost' => 6,  'Revenue' => 16, 'SellPrice' => 26],
+            ['Id' => 7,  'Cost' => 7,  'Revenue' => 17, 'SellPrice' => 27],
+            ['Id' => 8,  'Cost' => 8,  'Revenue' => 18, 'SellPrice' => 28],
+            ['Id' => 9,  'Cost' => 9,  'Revenue' => 19, 'SellPrice' => 29],
+            ['Id' => 10, 'Cost' => 10, 'Revenue' => 20, 'SellPrice' => 30],
+            ['Id' => 11, 'Cost' => 11, 'Revenue' => 21, 'SellPrice' => 31],
         ];
         $row_per_group = 4;
         $field_name = 'Revenue';
