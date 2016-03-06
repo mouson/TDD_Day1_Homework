f37d81b1e17ca56dc0887c5c880391d984228eb6
mouson <mouson@gmail.com>
2016-03-07 01:17:24 +0800

    新增測試：是否如輸入之參數針對集合分組

--- tests/GroupSplitTool/GroupSplitToolTest.php
+++ tests/GroupSplitTool/GroupSplitToolTest.php
@@ -5,6 +5,60 @@ class GroupSplitToolTest extends PHPUnit_Framework_TestCase
 
     /**
      * 分頁取值工具
+     * 測試以三筆為一分頁，驗證分組結果是否為四組
+     * @group GroupSplitTool
+     */
+    public function test_GroupSplitTool_3筆為一組_驗證分組結果是否為共4組()
+    {
+        /** Arrange */
+        $sample_data = [
+            ['Id' => 1,  'Cost' => 1,  'Revenue' => 11, 'SellPrice' => 21],
+            ['Id' => 2,  'Cost' => 2,  'Revenue' => 12, 'SellPrice' => 22],
+            ['Id' => 3,  'Cost' => 3,  'Revenue' => 13, 'SellPrice' => 23],
+            ['Id' => 4,  'Cost' => 4,  'Revenue' => 14, 'SellPrice' => 24],
+            ['Id' => 5,  'Cost' => 5,  'Revenue' => 15, 'SellPrice' => 25],
+            ['Id' => 6,  'Cost' => 6,  'Revenue' => 16, 'SellPrice' => 26],
+            ['Id' => 7,  'Cost' => 7,  'Revenue' => 17, 'SellPrice' => 27],
+            ['Id' => 8,  'Cost' => 8,  'Revenue' => 18, 'SellPrice' => 28],
+            ['Id' => 9,  'Cost' => 9,  'Revenue' => 19, 'SellPrice' => 29],
+            ['Id' => 10, 'Cost' => 10, 'Revenue' => 20, 'SellPrice' => 30],
+            ['Id' => 11, 'Cost' => 11, 'Revenue' => 21, 'SellPrice' => 31],
+        ];
+
+        $row_per_group = 3;
+
+        $target = new GroupSplitTool($sample_data, $row_per_group);
+
+        $expected_group_count = 4;
+        $expected = [
+            [
+                ['Id' => 1,  'Cost' => 1,  'Revenue' => 11, 'SellPrice' => 21],
+                ['Id' => 2,  'Cost' => 2,  'Revenue' => 12, 'SellPrice' => 22],
+                ['Id' => 3,  'Cost' => 3,  'Revenue' => 13, 'SellPrice' => 23]
+            ], [
+                ['Id' => 4,  'Cost' => 4,  'Revenue' => 14, 'SellPrice' => 24],
+                ['Id' => 5,  'Cost' => 5,  'Revenue' => 15, 'SellPrice' => 25],
+                ['Id' => 6,  'Cost' => 6,  'Revenue' => 16, 'SellPrice' => 26],
+            ], [
+                ['Id' => 7,  'Cost' => 7,  'Revenue' => 17, 'SellPrice' => 27],
+                ['Id' => 8,  'Cost' => 8,  'Revenue' => 18, 'SellPrice' => 28],
+                ['Id' => 9,  'Cost' => 9,  'Revenue' => 19, 'SellPrice' => 29],
+            ], [
+                ['Id' => 10, 'Cost' => 10, 'Revenue' => 20, 'SellPrice' => 30],
+                ['Id' => 11, 'Cost' => 11, 'Revenue' => 21, 'SellPrice' => 31],
+            ]
+        ];
+
+        /** Act */
+        $actual = $target->getGroupList();
+
+        /** Assert */
+        $this->assertCount($expected_group_count, $actual);
+        $this->assertArraySubset($expected, $actual);
+    }
+
+    /**
+     * 分頁取值工具
      * 測試以三筆為一分頁，分頁後，取個分頁的 Cost 總和
      * @group GroupSplitTool
      */
